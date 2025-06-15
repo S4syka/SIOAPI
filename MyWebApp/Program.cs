2ae6ad9ed87fcfddaaebc87a3596c7c127c23447
Erekle Khomasuridze <93726684+S4syka@users.noreply.github.com>
2025-06-15 18:40:33 +0400

    Add FastEndpoints.Swagger and update API documentation
    - Updated `MyWebApp.csproj` to include `FastEndpoints.Swagger` v6.1.0 for enhanced API documentation.
    - Modified `Program.cs` to replace default Swagger methods with `FastEndpoints.Swagger` configuration.
    - Changed `ITestImageRepository` implementation from mock to concrete repository.
    - Updated Swagger middleware to use `UseSwaggerUi` and added `UseSwaggerGen` for proper documentation generation.

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,20 +1,22 @@
 using Contracts;
 using FastEndpoints;
+using FastEndpoints.Swagger;
 using Model.Models;
 using Repository;
 using Repository.S3;
 
 var bld = WebApplication.CreateBuilder(args);
 bld.Services.AddFastEndpoints();
-bld.Services.AddEndpointsApiExplorer();
-bld.Services.AddSwaggerGen();
+//bld.Services.AddEndpointsApiExplorer();
+bld.Services.AddSwaggerDocument();
 bld.Services.AddScoped<OaDbContext>();
 bld.Services.AddScoped<RepositoryManager>();
-bld.Services.AddScoped<ITestImageRepository, TestImageMockRepository>();
+bld.Services.AddScoped<ITestImageRepository, TestImageRepository>();
 //bld.Services.AddSingleton<ILogger, MyWebApp.Logger>();
 
 var app = bld.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseFastEndpoints();
+app.UseSwaggerUi();
+app.UseFastEndpoints()
+    .UseSwaggerGen(); //add this
+
 app.Run();
