ae2ddb08532acb5d22a4c5db6dbd9c8842d32c0a
Erekle Khomasuridze <93726684+S4syka@users.noreply.github.com>
2025-06-16 08:30:11 +0400

    Revert "Add FastEndpoints.Swagger and update API documentation"
    This reverts commit 2ae6ad9ed87fcfddaaebc87a3596c7c127c23447.

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,22 +1,20 @@
 using Contracts;
 using FastEndpoints;
-using FastEndpoints.Swagger;
 using Model.Models;
 using Repository;
 using Repository.S3;
 
 var bld = WebApplication.CreateBuilder(args);
 bld.Services.AddFastEndpoints();
-//bld.Services.AddEndpointsApiExplorer();
-bld.Services.AddSwaggerDocument();
+bld.Services.AddEndpointsApiExplorer();
+bld.Services.AddSwaggerGen();
 bld.Services.AddScoped<OaDbContext>();
 bld.Services.AddScoped<RepositoryManager>();
-bld.Services.AddScoped<ITestImageRepository, TestImageRepository>();
+bld.Services.AddScoped<ITestImageRepository, TestImageMockRepository>();
 //bld.Services.AddSingleton<ILogger, MyWebApp.Logger>();
 
 var app = bld.Build();
-app.UseSwaggerUi();
-app.UseFastEndpoints()
-    .UseSwaggerGen(); //add this
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseFastEndpoints();
 app.Run();
